9d8f4d2a28e8dbde8f26f536df5bc05cf1b60c50
Rodrigo <rodrigo.frazao@cegeka.com>
2017-03-06 12:40:07 +0100

    User Test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,13 +1,24 @@
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.List;
+
 public class UserTest {
 
     @Test
     public void userAsAPieceOfCandy(){
 
         User dieter = new User("Dieter",new Candy("gummyBears", 5, 1.20));
-        Assertions.assertThat(dieter.getName()).Equals("Dieter");
-       
+        Assertions.assertThat(dieter.getName().Equals("Dieter"));
+        Assertions.assertThat(dieter.getCandy().getName().equals("gummyBears"));
+    }
+
+    @Test
+    public void userAsACandyBag(){
+        List<Candy> candyBag = new List<Candy>();
+        Candy candy = new Candy("gummyBears", 5, 1.20);
+        candyBag.add(candy);
+        User dieter = new User("Dieter",candyBag);
+        Assertions.assertThat(dieter.getCandyBag()).contains(candy);
     }
 }
