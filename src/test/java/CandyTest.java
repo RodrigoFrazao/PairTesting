9de1b3fca0290b26d9de8f441d0d1e2ac925efb1
Rodrigo <rodrigo.frazao@cegeka.com>
2017-03-06 11:21:14 +0100

    first Test

--- src/test/java/CandyTest.java
+++ src/test/java/CandyTest.java
@@ -1,14 +1,28 @@
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CandyTest {
 
-    @Test
-    public void candyConstructorTest(){
 
+    @Before
+    public void setupTest(){
         Candy candy = new Candy("gummyBears",5,1.20);
+    }
+
+
+    @Test
+    public void candyNameTest(){
         Assertions.assertThat(candy.getName().equals("gummyBears"));
+    }
+
+    @Test
+    public void candyPriceTest(){
         Assertions.assertThat(candy.getPrice().equals(5));
+    }
+
+    @Test
+    public void candyWeightTest(){
         Assertions.assertThat(candy.getWeight().equals(1.20));
     }
 }
